e4e79830041d98eabb9ab6173cd4901635289488
danielgraham <dpgraham.85@gmail.com>
2014-03-19 01:07:34 -0700

    Fixing travis some more

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     grunt.initConfig({
 
@@ -11,8 +12,32 @@ module.exports = function(grunt) {
             files: "src/*.js"
         },
 
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: "../dataDashAction"
+                }
+            },
+
+            keepalive: {
+                options: {
+                    port: 9000,
+                    base: "../dataDashAction",
+                    keepalive: true
+                }
+            }
+        },
+
+
         qunit: {
-            all: ["tests/base.html"]
+            all: {
+                options: {
+                    urls: [
+                        "http://localhost:9001/tests/base.html"
+                    ]
+                }
+            }
         },
 
         uglify: {
@@ -29,12 +54,9 @@ module.exports = function(grunt) {
                 }]
             }
         }
-
-
-
     });
 
     // Default task(s).
-    grunt.registerTask('travis', ['jshint', 'qunit']);
+    grunt.registerTask('travis', ['connect:server', 'jshint', 'qunit']);
 
 };
